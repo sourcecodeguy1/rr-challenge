a297009f3ce400aed75ca525468712d183f87075
sourcecodeguy1 <sandovaljulio2@yahoo.com>
2021-04-17 00:44:39 -0700

    add csrf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script>window.Laravel = {csrf: '{{ csrf_token() }}'} </script>
+
         <title>Laravel</title>
 
         <!-- Bootstrap CSS -->
